1b5d6defb9c6735f0d31614fef9fa092501f04f8
vinnaver8 <francisvinnaver8@gmail.com>
2025-05-18 14:42:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,53 +3,21 @@ window.onload = () => {
   anime.timeline()
     .add({
       targets: '#cursor1',
-      translateX: [ -100, 40 ],
-      translateY: [ -100, 10 ],
-      rotate: 45,
+      translateX: [10, 40],
+      translateY: [-45, 10],
+      rotate: [0, 45],
       duration: 1500,
       easing: 'easeOutExpo'
-    })
-    .add({
-      targets: '#cursor1',
-      translateX: 20,
-      translateY: -30,
-      rotate: 20,
-      duration: 1000,
-      easing: 'easeOutQuad'
-    })
-    .add({
-      targets: '#cursor1',
-      translateX: 10,
-      translateY: -45,
-      rotate: 0,
-      duration: 800,
-      easing: 'easeOutQuad'
     });
 
   // Cursor 2 (Lillanna)
   anime.timeline({ delay: 200 })
     .add({
       targets: '#cursor2',
-      translateX: [ 300, 160 ],
-      translateY: [ -150, 20 ],
-      rotate: -20,
+      translateX: [200, 160],
+      translateY: [-45, 20],
+      rotate: [0, -20],
       duration: 1800,
       easing: 'easeOutExpo'
-    })
-    .add({
-      targets: '#cursor2',
-      translateX: 180,
-      translateY: -30,
-      rotate: -10,
-      duration: 1000,
-      easing: 'easeOutQuad'
-    })
-    .add({
-      targets: '#cursor2',
-      translateX: 200,
-      translateY: -45,
-      rotate: 0,
-      duration: 800,
-      easing: 'easeOutQuad'
     });
 };
