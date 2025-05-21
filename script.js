7f01c73b3a4d391da6ba118ee0a7ed663f79d84b
vinnaver8 <francisvinnaver8@gmail.com>
2025-05-21 17:51:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
  const card = document.getElementById('card-wrapper');
   let direction = 1;
-  let position = 150; // start at 150px
-  const minY = 150;
-  const maxY = 700;
+  let position = 100; // start at 150px
+  const minY = 100;
+  const maxY = 1500;
   const speed = 0.8; // adjust for slower/faster
 
   function animate() {
