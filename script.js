c214fa3f71d375cf86097a0e4fad4ce7a26d8de5
vinnaver8 <francisvinnaver8@gmail.com>
2025-05-31 19:07:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
- const card = document.getElementById('card-wrapper');
-  let direction = 1;
-  let position = 100; // start at 150px
-  const minY = 100;
-  const maxY = 1200;
-  const speed = 0.8; // adjust for slower/faster
-
-  function animate() {
-    position += direction * speed;
-
-    if (position >= maxY || position <= minY) {
-      direction *= -1; // reverse direction
-    }
-
-    card.style.top = position + "px";
-    requestAnimationFrame(animate);
-  }
 
   // Start animation when DOM is ready
   window.addEventListener('DOMContentLoaded', animate);
